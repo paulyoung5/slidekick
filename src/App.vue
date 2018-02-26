b02955049aad7d29f6b4ae27ff68a1b9b763967f
Daniel Waghorn <daniel@daniel-waghorn.co.uk>
2018-02-26 15:24:38 +0000

    Set up Vue Auth [WIP]

--- src/App.vue
+++ src/App.vue
@@ -92,6 +92,18 @@ export default {
   methods: {
     ...mapActions(['setPageLoading'])
   },
+  created () {
+    this.$auth.login({
+      rememberMe: false,
+      redirect: null,
+      success () {
+        this.refreshUser().then(() => this.setPageLoading(false))
+      },
+      error () {
+        this.setPageLoading(false)
+      }
+    })
+  },
   watch: {
     '$route': function (route) {
       if (route.meta.showLoading) {
