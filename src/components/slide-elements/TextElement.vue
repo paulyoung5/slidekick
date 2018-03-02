4f07c1e7ae977224e784f977482b954d0028ade3
Paul Young <paul-young@live.co.uk>
2018-03-02 16:22:39 +0000

    Added ability to change X, Y, font family, font size of text elements

--- src/components/slide-elements/TextElement.vue
+++ src/components/slide-elements/TextElement.vue
@@ -1,31 +1,21 @@
 <style>
-
+  text:hover {
+    cursor: default;
+  }
 </style>
 
 <template>
-  <g class="hoverableElement">
-    <rect
-      ref="containerEl"
-      class="hoverState"
-      :width="containerWidth"
-      :height="containerHeight"
-      :x="containerX"
-      :y="containerY"
-    />
-
-    <text
-      ref="textEl"
-      dominant-baseline="text-before-edge"
-      :x="textX"
-      :y="textY"
-      :font-family="fontFamily"
-      :font-size="fontSize"
-      @mousedown="mousedown"
-      @click="inspectElement(element.id)"
-    >
-      {{ content }}
-    </text>
-  </g>
+  <text
+    ref="textEl"
+    dominant-baseline="text-before-edge"
+    :x="textX"
+    :y="textY"
+    :font-family="fontFamily"
+    :font-size="fontSize"
+    @click="inspectElement(element.id)"
+  >
+    {{ content }}
+  </text>
 </template>
 
 <script>
@@ -36,16 +26,15 @@ export default {
   props: {element: Object},
   data () {
     return {
-      textEl: null,
-      containerEl: null
+      textEl: null
     }
   },
   computed: {
     textX () {
-      return Number(this.element.properties.x.substr(0, this.element.properties.x.length - 2))
+      return this.element.properties.x
     },
     textY () {
-      return Number(this.element.properties.y.substr(0, this.element.properties.y.length - 2))
+      return this.element.properties.y
     },
     textHeight () {
       if (this.textEl) {
@@ -88,35 +77,10 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['inspectElement']),
-    mousedown (event) {
-      const svgEl = document.querySelector('svg')
-
-      svgEl.addEventListener('mousemove', this.mousemove)
-      svgEl.addEventListener('mouseup', this.mouseup)
-    },
-    mousemove (event) {
-      const bounding = this.textEl.getBoundingClientRect()
-
-      const newX = this.textX + (event.clientX - bounding.x)
-      const newY = this.textY + (event.clientY - bounding.y)
-
-      const newXformatted = newX > 0 ? Math.floor(newX) : Math.ceil(newX)
-      const newYformatted = newY > 0 ? Math.floor(newY) : Math.ceil(newY)
-
-      this.element.properties.x = `${newXformatted}px`
-      this.element.properties.y = `${newYformatted}px`
-    },
-    mouseup (event) {
-      const svgEl = document.querySelector('svg')
-
-      svgEl.removeEventListener('mouseup', this.mouseup)
-      svgEl.removeEventListener('mousemove', this.mousemove)
-    }
+    ...mapActions('editor', ['inspectElement'])
   },
   mounted () {
     this.textEl = this.$refs.textEl
-    this.containerEl = this.$refs.containerEl
   }
 }
 </script>
